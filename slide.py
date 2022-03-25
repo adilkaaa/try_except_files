428fcdc5fa0059752d229868220022c6a283f11d
adilkaaa <adilkhanakakov@gmail.com>
2022-03-25 12:16:44 +0600

    add try_except_files

---
+++ slide.py
@@ -0,0 +1,58 @@
+#1
+# a = int(input('number: '))
+# b = '3'
+#TypeError
+# print(a+b)
+
+
+#IndexError
+# print(b[399])
+
+#NameError
+# print(df)
+
+
+    
+
+#2
+#Code #1:
+
+# try:
+#     at = 10
+#     iin = 15
+#     wo = 20
+#     for e in range(-at, at):
+#         print(wo / e)
+#     if at > '5':
+#         print(at > 5)
+# except TypeError:
+#     print('ошибка типов')
+# except ZeroDivisionError:
+#     print('cant divide 0')
+
+#3
+#Code #2:
+
+# lst = []
+# try:
+#     raise NameError
+#     for i in renge(10):
+#         lst.apend(i)
+#     a = list(revesed(lst))
+#     sls_obj = slice('0','8')
+#     print(a[sls_obj])
+# except NameError:
+#     print('Ошибка имен')
+
+
+#4
+#Code #3:
+# try:
+#     a = (0)
+#     b = (1)
+#     numbers = []
+#     while b > a:
+#         numbers += b
+#         b += 1
+# except TypeError:
+#     print('ошибка типов')
